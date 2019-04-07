414bba618de3f1f59539adad21133043fe258a02
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-04-07 21:30:10 +0200

    Add mongoose

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import 'dotenv/config'
+
+const { MONGO_URI } = process.env
+
 // Require the fastify framework and instantiate it
 const fastify = require('fastify')({
   logger: true,
 })
+
+const mongoose = require('mongoose')
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected…'))
+  .catch(err => console.log(err))
 
 fastify.get('/', async (request, reply) => ({ hello: 'world' }))
 
