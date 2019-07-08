ebee6025d23dfcd7a345d8105bc557d5a3b7cdd5
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-07-08 23:28:57 +0200

    Refactor src/index into app & server

--- src/server.js
+++ src/server.js
@@ -1,25 +1,6 @@
-import express from 'express'
+import app from './app'
 import config from './config'
-import connect from './config/db'
-import middlewares from './config/middlewares'
-import routes from './config/routes'
-import errorHandler from './config/error_handler'
 import loggerFactory from './helpers/logger'
-
-// Instantiate express framework and apply middlewares
-const app = express()
-
-// Connect to database
-connect()
-
-// Add middlewares
-middlewares(app)
-
-// Import routes
-routes(app)
-
-// Add error handler
-errorHandler(app)
 
 // Create server
 const start = () => {
