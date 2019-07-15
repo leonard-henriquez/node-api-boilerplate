295faf21ec23fe8b64f1eca0d66c5d24c394e86c
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-07-15 21:25:48 +0200

    Refactor config for multi environment

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,21 @@
 require('dotenv').config()
+const path = require('path')
 const convict = require('convict')
 const defaultConfig = require('./default_config')
 
 // Load config
 const config = convict(defaultConfig)
+
+// Load env config
+const appRoot = config.get('app_root')
+const env = config.get('env')
+const envConfigPath = path.join(appRoot, 'config', `${env}.json`)
+try {
+  config.loadFile(envConfigPath)
+} catch (err) {
+  // eslint-disable-next-line no-console
+  console.warn(`Missing file: ${envConfigPath}`)
+}
 
 // Perform validation
 config.validate({ allowed: 'strict' })
