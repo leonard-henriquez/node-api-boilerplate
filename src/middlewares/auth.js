71f0aef0e0de74336d7a5509f91a14d3de781ed2
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-08-08 17:12:31 +0200

    Refactor auth coupling

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,13 @@
 const passport = require('passport')
 const { Strategy, ExtractJwt } = require('passport-jwt')
-const User = require('../resources/user/user.model')
 const config = require('../config')
 
 const verify = async (payload, next) => {
-  const user = await User.findById(payload.id)
-
-  if (user) {
-    return next(null, {
-      id: user.id,
-      email: user.email,
-    })
+  try {
+    return next(null, payload)
+  } catch (error) {
+    next(error)
   }
-
-  return next(null, false)
 }
 
 module.exports = () => {
