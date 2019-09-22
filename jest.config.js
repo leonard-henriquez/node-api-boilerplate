85e16ad54fa1d3e47cb9a533b4982ab37dd44371
Leonard-Henriquez <leonard.henriquez@edhec.com>
2019-09-22 23:10:07 +0200

    Remove Babel

--- jest.config.js
+++ jest.config.js
@@ -153,7 +153,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.[t|j]sx?$': 'babel-jest',
+    '^.+\\.[t|j]sx?$': 'ts-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
